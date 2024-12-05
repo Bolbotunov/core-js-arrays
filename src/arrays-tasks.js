b3c6b4cc939e56439ffeec9431f79b6360a73312
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-12-05 09:21:21 +0300

    feat: solved task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -43,8 +43,16 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxArr = arr1.length > arr2.length ? arr1 : arr2;
+  const minArr = arr1.length > arr2.length ? arr2 : arr1;
+  const totalArr = maxArr.map((item, index) => {
+    if (!minArr[index]) {
+      return item;
+    }
+    return item + minArr[index];
+  });
+  return totalArr;
 }
 
 /**
