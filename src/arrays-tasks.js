ed89d5121708bda09045fb919f6b8b181e0a8667
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-12-12 08:58:03 +0300

    feat:solcve task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -434,8 +434,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, ind) => {
+    return Array.from({ length: n }, (item, index) => {
+      return index === ind ? 1 : 0;
+    });
+  });
 }
 
 /**
